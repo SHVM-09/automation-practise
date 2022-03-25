f15e792d7bde9b228e9a94ed0c00772b761f3bc8
neelbrahmakshatriya <brahma.neel.j@gmail.com>
2022-03-25 15:23:11 +0530

    refactor(demo): refactor generate.sh file for pro demos

--- react/pro/demo-generation/generate.sh
+++ react/pro/demo-generation/generate.sh
@@ -3,7 +3,7 @@ set -e
 node generate-demo-configs.js
 
 # Demo generation loop
-for i in {1..6}
+for i in 1 2 3 4 5 6
 do
 # Replace necessary in src folder
 	node replace.js demo-$i
@@ -13,10 +13,7 @@ do
   yarn next export
 # Move the demo to root folder
   mv out ../../demo-$i
-  cd ../../../automation-scripts/react/pro/demo-generation
-# Zip and remove demo folder 
-  zip -r ../../../../master-react-mui-nextjs/demo-$i.zip ../../../../master-react-mui-nextjs/demo-$i
-  rm -rf ../../../../master-react-mui-nextjs/demo-$i
 # Reset the replaced content before
+  cd ../../../automation-scripts/react/pro/demo-generation
   node reset.js demo-$i
 done
