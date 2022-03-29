43721f3aa22dcdb4a9da3abbfbeba3faea17d399
ronaklalwaniii <ronak@pixinvent.com>
2022-03-29 09:24:09 +0530

    refactor(demo): adds promises to demo script

--- react/pro/demo-generation/reset.js
+++ react/pro/demo-generation/reset.js
@@ -73,145 +73,156 @@ const removeBasePathInImages = (dirPath, arrayOfFiles) => {
 
 // ** Reset replaced locales path
 const removeBasePathInI18n = () => {
-  fs.readFile(i18nPath, 'utf-8', (err, data) => {
-    if (err) {
-      console.log(err)
+  return new Promise(resolve => {
+    fs.readFile(i18nPath, 'utf-8', (err, data) => {
+      if (err) {
+        console.log(err)
 
-      return
-    } else {
-      const updatedData = data.replace(`${URL}/${demo}/locales/`, '/locales/')
-      fs.writeFile(i18nPath, '', err => {
-        if (err) {
-          console.log(err)
+        return
+      } else {
+        const updatedData = data.replace(`${URL}/${demo}/locales/`, '/locales/')
+        fs.writeFile(i18nPath, '', err => {
+          if (err) {
+            console.log(err)
 
-          return
-        } else {
-          fs.writeFile(i18nPath, updatedData, err => {
-            if (err) {
-              console.log(err)
+            return
+          } else {
+            fs.writeFile(i18nPath, updatedData, err => {
+              if (err) {
+                console.log(err)
 
-              return
-            }
-          })
-        }
-      })
-    }
+                return
+              }
+            })
+          }
+        })
+      }
+    })
+
+    resolve()
   })
 }
 
 removeBasePathInImages(`${pathConfig.fullVersionTSXPath}/src`)
 removeBasePathInI18n()
-
-// ** Reset replaced settings in localStorage if settingsContextFile exist
-if (fs.existsSync(settingsContextFile)) {
-  fs.readFile(settingsContextFile, 'utf-8', (err, data) => {
-    if (err) {
-      console.log(err)
-
-      return
-    } else {
-      const result = data.replace(
-        new RegExp(/(localStorage.(get|set)Item\(')(.*)('.*\))/, 'g'),
-        `$1settings$4`
-      )
-      fs.writeFile(settingsContextFile, '', err => {
+  .then(() => {
+    // ** Reset replaced settings in localStorage if settingsContextFile exist
+    if (fs.existsSync(settingsContextFile)) {
+      fs.readFile(settingsContextFile, 'utf-8', (err, data) => {
         if (err) {
           console.log(err)
 
           return
         } else {
-          fs.writeFile(settingsContextFile, result, function (err) {
+          const result = data.replace(
+            new RegExp(/(localStorage.(get|set)Item\(')(.*)('.*\))/, 'g'),
+            `$1settings$4`
+          )
+          fs.writeFile(settingsContextFile, '', err => {
             if (err) {
               console.log(err)
 
               return
+            } else {
+              fs.writeFile(settingsContextFile, result, function (err) {
+                if (err) {
+                  console.log(err)
+
+                  return
+                }
+              })
             }
           })
         }
       })
     }
   })
-}
+  .then(() => {
+    // ** Reset replaced basePath if nextConfigPath exist
+    if (fs.existsSync(nextConfigPath)) {
+      const nextConfigData = fs
+        .readFileSync(nextConfigPath)
+        .toString()
+        .split('\n')
 
-// ** Reset replaced basePath if nextConfigPath exist
-if (fs.existsSync(nextConfigPath)) {
-  const nextConfigData = fs.readFileSync(nextConfigPath).toString().split('\n')
+      const result = nextConfigData
+        .filter(line => {
+          return line.indexOf('basePath') === -1
+        })
+        .join('\n')
 
-  const result = nextConfigData
-    .filter(line => {
-      return line.indexOf('basePath') === -1
-    })
-    .join('\n')
+      fs.writeFile(nextConfigPath, result, err => {
+        if (err) {
+          console.log(err)
 
-  fs.writeFile(nextConfigPath, result, err => {
-    if (err) {
-      console.log(err)
-
-      return
+          return
+        }
+      })
+    } else {
+      console.log('NextConfig file Does Not Exists')
     }
   })
-} else {
-  console.log('NextConfig file Does Not Exists')
-}
-
-// ** Reset replaced themeConfig if themeConfigPath & demoConfigPath exist
-if (fs.existsSync(themeConfigPath) && fs.existsSync(demoConfigPath)) {
-  fs.readFile(demoConfigPath, 'utf-8', (err, data) => {
-    if (err) {
-      console.log(err)
-
-      return
-    } else {
-      fs.writeFile(themeConfigPath, '', err => {
+  .then(() => {
+    // ** Reset replaced themeConfig if themeConfigPath & demoConfigPath exist
+    if (fs.existsSync(themeConfigPath) && fs.existsSync(demoConfigPath)) {
+      fs.readFile(demoConfigPath, 'utf-8', (err, data) => {
         if (err) {
           console.log(err)
 
           return
         } else {
-          fs.writeFile(themeConfigPath, data, err => {
+          fs.writeFile(themeConfigPath, '', err => {
             if (err) {
               console.log(err)
 
               return
+            } else {
+              fs.writeFile(themeConfigPath, data, err => {
+                if (err) {
+                  console.log(err)
+
+                  return
+                }
+              })
             }
           })
         }
       })
+    } else {
+      console.log('themeConfigPath file & demoConfigPath file Does Not Exists')
     }
   })
-} else {
-  console.log('themeConfigPath file & demoConfigPath file Does Not Exists')
-}
+  .then(() => {
+    // ** Adds Test to components & Form Elements
+    const resetTestFolders = () => {
+      const resetPromise = testFoldersToModify.map(folder => {
+        return new Promise(resolve => {
+          if (fs.existsSync(folder.to)) {
+            copyDirectory(folder.to, folder.from)
+          }
 
-// ** Adds Test to components & Form Elements
-const resetTestFolders = () => {
-  const resetPromise = testFoldersToModify.map(folder => {
-    return new Promise(resolve => {
-      if (fs.existsSync(folder.to)) {
-        copyDirectory(folder.to, folder.from)
-      }
-
-      resolve()
-    })
-  })
-
-  Promise.all(resetPromise)
-    .then(() => {
-      testFoldersToCopy.map(folder => {
-        if (fs.existsSync(folder.to)) {
-          copyDirectory(folder.to, folder.from)
-        }
+          resolve()
+        })
       })
-    })
-    .then(() => {
-      if (fs.existsSync('./temp-folder')) {
-        fs.rm('./temp-folder', { recursive: true }, err => {
-          if (err) {
-            console.log(err)
+
+      Promise.all(resetPromise)
+        .then(() => {
+          testFoldersToCopy.map(folder => {
+            if (fs.existsSync(folder.to)) {
+              copyDirectory(folder.to, folder.from)
+            }
+          })
+        })
+        .then(() => {
+          if (fs.existsSync('./temp-folder')) {
+            fs.rm('./temp-folder', { recursive: true }, err => {
+              if (err) {
+                console.log(err)
+              }
+            })
           }
         })
-      }
-    })
-}
+    }
 
-resetTestFolders()
+    resetTestFolders()
+  })
