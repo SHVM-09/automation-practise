6ae7ac6ececffa92d124ebe7de4e9b5ca5168e5b
ronaklalwaniii <ronak@pixinvent.com>
2022-05-23 08:31:36 +0530

    feat(prettier): Adds prettier file for react

---
+++ react/.prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  semi: false,
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 120,
+  singleQuote: true,
+  insertPragma: false,
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  jsxSingleQuote: true,
+  requirePragma: false,
+  proseWrap: 'preserve',
+  trailingComma: 'none',
+  bracketSameLine: false,
+  quoteProps: 'as-needed',
+  htmlWhitespaceSensitivity: 'css',
+}
