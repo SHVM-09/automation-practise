b3bacdd1828aa6d375ea53fb6e7600e777b7c2f8
JD Solanki <jdsolanki0001@gmail.com>
2022-09-28 16:53:26 +0530

    [vue]: wip - just before sfc compilation

--- vue-ts/src/templates/base/genJS.ts
+++ vue-ts/src/templates/base/genJS.ts
@@ -1,154 +1,21 @@
 import { TempLocation } from '@/utils/temp';
-import { parse } from 'acorn';
-import { namedTypes } from 'ast-types';
-import chalk from 'chalk';
 import { execSync } from 'child_process';
-import { generate } from 'escodegen';
 import fs from 'fs-extra';
+import { globby } from 'globby';
 import path from 'path';
 import { TemplateBaseConfig } from './config';
 
-// We need this function for `{ format: { escapeless: true } }` escodegen option to preserve the emojis while generating the code from AST node
-const generateFromAST = (ast: unknown) => generate(ast, {
-    indent: {
-        // indent using two spaces
-        style: '  ',
-    },
-    format: { escapeless: true }
-})
-
-// ℹ️ https://github.com/tcr/acorn.ts#note-on-using-with-escodegen
-const acornParse = <T>(str: string) => {
-  const comments: {type: string, value: string, range: [number, number]}[] = [], tokens: { range: [number, number] }[] = [];
-
-  const ast: T = parse(str, {
-    ecmaVersion: 'latest',
-    'sourceType': 'module',
-
-    ranges: true,
-    // collect comments in Esprima's format
-    onComment: function (block, text, start, end) {
-      comments.push({
-        type: block ? 'Block' : 'Line',
-        value: text,
-        range: [start, end]
-      });
-    },
-    // collect token ranges
-    onToken: function (token) {
-      tokens.push({
-        range: [token.start, token.end]
-      });
-    }
-  }) as T
-
-  return {
-    ast, comments, tokens
-  }
-}
-
-class ViteConfigHandler {
-  private ast: namedTypes.Program | undefined
-  
-  constructor(private viteConfig: string) {}
-
-  insertPluginProperty(ast: namedTypes.CallExpression, str?: string, astToInsert?: namedTypes.Property) {
-    let _astToInsert = astToInsert
-    if (str) {
-      _astToInsert = (
-        (
-          (acornParse(`export default {${str}}`).ast as namedTypes.Program).body[0] as namedTypes.ExportDefaultDeclaration
-        ).declaration as namedTypes.ObjectExpression
-      ).properties[0] as namedTypes.Property
-    }
-    (ast.arguments as namedTypes.ObjectExpression[])[0].properties.push((_astToInsert as namedTypes.Property))
-
-    return ast
-  }
-
-  private getPluginPropertyAST() {
-    if (!this.ast)
-      throw new Error(chalk.redBright('AST is not created yet!'));
-
-    const exportDefaultDeclaration = this.ast.body.find(declaration => declaration.type === 'ExportDefaultDeclaration') as namedTypes.ExportDefaultDeclaration;
-    const viteConfigObjectProperties = (
-      (exportDefaultDeclaration.declaration as namedTypes.CallExpression)
-        .arguments[0] as namedTypes.ObjectExpression
-    ).properties;
-
-    const pluginsProperty = viteConfigObjectProperties.find(property => property.type === 'Property'
-      && (property.key as namedTypes.Identifier).name === 'plugins'
-    ) as namedTypes.Property;
-    return pluginsProperty;
-  }
-
-  getPluginIndexInPluginPropertyAST(pluginName: string) {
-    const pluginsProperty = this.getPluginPropertyAST()
-
-    return (pluginsProperty.value as namedTypes.ArrayExpression)
-      .elements.findIndex(e =>
-        (
-          (e as namedTypes.CallExpression).callee as namedTypes.Identifier)
-          .name === pluginName
-      )
-  }
-
-  replacePluginAST(pluginName: string, ast: unknown) {
-    const pluginIndex = this.getPluginIndexInPluginPropertyAST(pluginName)
-
-    // ❗ WIP
-  }
-
-  getPluginAST(name: string) {
-    const pluginsProperty = this.getPluginPropertyAST();
-
-    return (pluginsProperty.value as namedTypes.ArrayExpression)
-      .elements.find(e =>
-        (
-          (e as namedTypes.CallExpression).callee as namedTypes.Identifier)
-          .name === name
-      ) as namedTypes.CallExpression | undefined
-  }
-
-  updateExtensionsToJS() {
-    this.viteConfig = this.viteConfig.replace('themeConfig.ts', 'themeConfig.js')
-  }
-
-  private transformToAST() {
-    if (!this.ast) this.ast = acornParse<namedTypes.Program>(this.viteConfig).ast
-  }
-
-  addEslintConfigInAutoImportPlugin() {
-    this.transformToAST()
-    
-    const autoImportAST = this.getPluginAST('AutoImport')
-
-    if (!autoImportAST) throw new Error(chalk.redBright("Can't find AutoImport plugin!"))
-    
-    const eslintConfig = `eslintrc: {
-        enabled: true,
-        filepath: './.eslintrc-auto-import.json',
-    }`
-
-    this.updatePluginAST('AutoImport', this.insertPluginProperty(autoImportAST, eslintConfig))
-  }
-
-  updatePluginAST(pluginName: string, ast: namedTypes.CallExpression) {
-    throw new Error('Method not implemented.');
-  }
-}
-
 export class GenJS {
-  private projectSrcPath: string
+  // private projectSrcPath: string
   private tempDir: string
 
   constructor(private templateConfig: TemplateBaseConfig) {
-    this.projectSrcPath = path.join(templateConfig.projectPath, 'src')
+    // this.projectSrcPath = path.join(templateConfig.projectPath, 'src')
     this.tempDir = new TempLocation().tempDir
   }
 
   private genProjectCopyCommand(): string {
-    let command = `rsync -av --progress ${this.templateConfig.paths.tSFull}/* ${this.tempDir} `
+    let command = `rsync -av --progress ${this.templateConfig.paths.tSFull}/ ${this.tempDir} `
     this.templateConfig.packageCopyIgnorePatterns.forEach(pattern => {
       
       // We need to escape the * when using rsync
@@ -169,22 +36,97 @@ export class GenJS {
   private updateViteConfig() {
     const viteConfigPath = path.join(this.tempDir, 'vite.config.ts')
 
-    console.log('viteConfigPath :>> ', viteConfigPath);
-
     let viteConfig = fs.readFileSync(viteConfigPath, { encoding: 'utf-8' })
 
-    const viteConfigHandler = new ViteConfigHandler(viteConfig)
+    // Replace themeConfig.ts alias to themeConfig.js
+    viteConfig = viteConfig.replace('themeConfig.ts', 'themeConfig.js')
 
-    viteConfigHandler.updateExtensionsToJS()
+    // enable eslintrc in AutoImport plugin
+    const autoImportEslintConfig = `eslintrc: {
+        enabled: true,
+        filepath: './.eslintrc-auto-import.json',
+    },`
 
-    viteConfigHandler.addEslintConfigInAutoImportPlugin()
+    viteConfig = viteConfig.replace(/(AutoImport\({\n(\s+))/, `$1${autoImportEslintConfig}\n$2`)
+    
+    fs.writeFileSync(viteConfigPath, viteConfig, { encoding: 'utf-8' })
   }
 
-  genJS() {
+  private updateEslintConfig() {
+    const eslintConfigPath = path.join(this.tempDir, '.eslintrc.js')
+    const viteConfigPath = path.join(this.tempDir, 'vite.config.ts')
+    
+    // Add import resolver package
+    execSync('yarn add eslint-import-resolver-alias', { cwd: this.tempDir })
+
+    // Read eslint config
+    let eslintConfig = fs.readFileSync(eslintConfigPath, { encoding: 'utf-8' })
+
+    /*
+      Remove all the lines which contains word 'typescript' or 'antfu'
+      ℹ️ We will remove line that contains word 'antfu' => We need to remove antfu eslint config as this also add TS rules
+    */
+    eslintConfig = eslintConfig.split('\n')
+      .filter(line => !(line.includes('typescript') || line.includes('antfu')))
+      .join('\n')
+
+    // Remove eslint internal rules
+    eslintConfig = eslintConfig.replace(/(\s+\/\/ Internal Rules|\s+'valid-appcardcode.*)/g, '')
+
+    /*
+      Add auto-import json file in extends array
+      Regex: https://regex101.com/r/1RYdYv/2
+    */
+      eslintConfig = eslintConfig.replace(/(extends: \[\n(\s+))/g, `$1'.eslintrc-auto-import.json',\n$2`)
+
+    // Add vite aliases in eslint import config
+    const viteConfig = fs.readFileSync(viteConfigPath, { encoding: 'utf-8' })
+    const importAliases = viteConfig.matchAll(/'(?<alias>.*)': fileURLToPath\(new URL\('(?<path>.*)',.*,/g)
+    const importAliasesEslintConfig = `alias: {'extensions': ['.ts', '.js', '.tsx', '.jsx', '.mjs'], 'map': ${JSON.stringify([...importAliases].map(m => m.groups && Object.values(m.groups)))}}`
+
+    eslintConfig = eslintConfig.replace(/(module\.exports = {(\n|.)*\s{6}},)((\n|.)*)/g, `$1${importAliasesEslintConfig}$3`)
+
+    fs.writeFileSync(eslintConfigPath, eslintConfig, { encoding: 'utf-8' })
+  }
+
+  private updateTSConfig() {
+    const tsConfigPath = path.join(this.tempDir, 'tsconfig.json')
+
+    const tsConfig = fs.readJsonSync(tsConfigPath)
+
+    tsConfig.compilerOptions.sourceMap = false
+
+    fs.writeJsonSync(tsConfigPath, tsConfig)
+  }
+
+  async genJS() {
     // Copy project to temp dir
     this.copyTSFullToTempDir()
 
     // Update vite config
     this.updateViteConfig()
+
+    // update eslint
+    this.updateEslintConfig()
+
+    /*
+      ℹ️ Now we will generate the js & jsx files from ts & tsx files
+      But for this we need some changes in our jsconfig file:
+        1. Disable source maps
+    */
+    this.updateTSConfig()
+
+    /*
+      Install packages
+      ℹ️ We need this to run tsc & generate build
+    */
+    execSync('yarn', { cwd: this.tempDir })
+
+    // Run `tsc` to compile TypeScript files
+    execSync(`yarn tsc`, { cwd: this.tempDir })
+
+    // Remove all TypeScript files
+    const tSFiles = await globby(['*.ts', '*.tsx'], { cwd: this.tempDir, absolute: true });
+    tSFiles.forEach(f => fs.removeSync(f))
   }
 }
