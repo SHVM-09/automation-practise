22a1e5dcbfec09fc4a9559eee56693c100c63ec3
JD Solanki <jdsolanki0001@gmail.com>
2022-10-18 20:31:19 +0530

    [vue] chore: removed old automation

--- vue/src/templates/master/template.py
+++
@@ -1,7 +0,0 @@
-from templates.base.template import TemplateBase
-from templates.master.settings import MasterConfig
-
-
-class Master(TemplateBase):
-    def __init__(self):
-        super().__init__(MasterConfig)
--- vue/src/templates/materio/template.py
+++
@@ -1,7 +0,0 @@
-from templates.base.template import TemplateBase
-from templates.materio.settings import MaterioConfig
-
-
-class Materio(TemplateBase):
-    def __init__(self):
-        super().__init__(MaterioConfig)
