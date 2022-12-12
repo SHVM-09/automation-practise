002164b12cb6fde02d116412ca8f484898538c43
alokVishu <alokpixinvent@gmail.com>
2022-12-12 15:31:53 +0530

    refactor: login page refactored

--- vue/src/templates/materio/data/pages/login.vue
+++ vue/src/templates/materio/data/pages/login.vue
@@ -41,18 +41,45 @@ const authThemeMask = useGenerateImageVariant(authV2MaskLight, authV2MaskDark)
       </h1>
     </div>
 
-    <VRow no-gutters class="auth-wrapper">
-      <VCol md="8" class="d-none d-md-flex align-center justify-center position-relative">
-        <div class="d-flex align-center justify-center w-100 pa-10 pe-0">
-          <VImg max-width="768px" :src="authThemeImg" class="auth-illustration" />
+    <VRow
+      no-gutters
+      class="auth-wrapper"
+    >
+      <VCol
+        md="8"
+        class="d-none d-md-flex align-center justify-center position-relative"
+      >
+        <div
+          class="d-flex align-center justify-center w-100 pa-10 pe-0"
+        >
+          <VImg
+            max-width="768px"
+            :src="authThemeImg"
+            class="auth-illustration"
+          />
         </div>
 
-        <VImg :width="276" :src="tree" class="auth-footer-start-tree" />
-        <VImg class="auth-footer-mask" :src="authThemeMask" />
+        <VImg
+          :width="276"
+          :src="tree"
+          class="auth-footer-start-tree"
+        />
+        <VImg
+          class="auth-footer-mask"
+          :src="authThemeMask"
+        />
       </VCol>
 
-      <VCol cols="12" md="4" class="auth-card-v2 d-flex align-center justify-center">
-        <VCard flat :max-width="500" class="mt-12 mt-sm-0 pa-4">
+      <VCol
+        cols="12"
+        md="4"
+        class="auth-card-v2 d-flex align-center justify-center"
+      >
+        <VCard
+          flat
+          :max-width="500"
+          class="mt-12 mt-sm-0 pa-4"
+        >
           <VCardText>
             <h5 class="text-h5 font-weight-semibold mb-1">
               Welcome to {{ themeConfig.app.title }}! 👋🏻
@@ -62,48 +89,76 @@ const authThemeMask = useGenerateImageVariant(authV2MaskLight, authV2MaskDark)
             </p>
           </VCardText>
           <VCardText>
-            <VForm @submit.prevent="() => { }">
+            <VForm @submit.prevent="() => {}">
               <VRow>
                 <!-- email -->
                 <VCol cols="12">
-                  <VTextField v-model="form.email" label="Email" type="email" />
+                  <VTextField
+                    v-model="form.email"
+                    label="Email"
+                    type="email"
+                  />
                 </VCol>
 
                 <!-- password -->
                 <VCol cols="12">
-                  <VTextField v-model="form.password" label="Password" :type="isPasswordVisible ? 'text' : 'password'"
+                  <VTextField
+                    v-model="form.password"
+                    label="Password"
+                    :type="isPasswordVisible ? 'text' : 'password'"
                     :append-inner-icon="isPasswordVisible ? 'mdi-eye-off-outline' : 'mdi-eye-outline'"
-                    @click:append-inner="isPasswordVisible = !isPasswordVisible" />
+                    @click:append-inner="isPasswordVisible = !isPasswordVisible"
+                  />
 
                   <div class="d-flex align-center flex-wrap justify-space-between mt-1 mb-4">
-                    <VCheckbox v-model="form.remember" label="Remember me" />
-                    <RouterLink class="text-primary ms-2 mb-1"
-                      :to="{ name: 'pages-authentication-forgot-password-v2' }">
+                    <VCheckbox
+                      v-model="form.remember"
+                      label="Remember me"
+                    />
+                    <a
+                      class="text-primary ms-2 mb-1"
+                      href="#"
+                    >
                       Forgot Password?
-                    </RouterLink>
+                    </a>
                   </div>
 
-                  <VBtn block type="submit">
+                  <VBtn
+                    block
+                    type="submit"
+                  >
                     Login
                   </VBtn>
                 </VCol>
 
                 <!-- create account -->
-                <VCol cols="12" class="text-center text-base">
+                <VCol
+                  cols="12"
+                  class="text-center text-base"
+                >
                   <span>New on our platform?</span>
-                  <RouterLink class="text-primary ms-2" :to="{ name: 'pages-authentication-register-v2' }">
+                  <a
+                    class="text-primary ms-2"
+                    href="#"
+                  >
                     Create an account
-                  </RouterLink>
+                  </a>
                 </VCol>
 
-                <VCol cols="12" class="d-flex align-center">
+                <VCol
+                  cols="12"
+                  class="d-flex align-center"
+                >
                   <VDivider />
                   <span class="mx-4">or</span>
                   <VDivider />
                 </VCol>
 
                 <!-- auth providers -->
-                <VCol cols="12" class="text-center">
+                <VCol
+                  cols="12"
+                  class="text-center"
+                >
                   <AuthProvider />
                 </VCol>
               </VRow>
