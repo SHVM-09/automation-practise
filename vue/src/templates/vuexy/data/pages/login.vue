8a40985c55f80884362050e4f62f7dfd5efad429
alokVishu <alokpixinvent@gmail.com>
2023-03-17 19:08:31 +0530

    refactor(vue): vuexy login page updated

--- vue/src/templates/vuexy/data/pages/login.vue
+++ vue/src/templates/vuexy/data/pages/login.vue
@@ -1,26 +1,31 @@
 <script setup lang="ts">
 import AuthProvider from '@/views/pages/authentication/AuthProvider.vue'
 import { useGenerateImageVariant } from '@core/composable/useGenerateImageVariant'
-import { VNodeRenderer } from '@layouts/components/VNodeRenderer'
-import { themeConfig } from '@themeConfig'
-import { emailValidator, requiredValidator } from '@validators'
-
 import authV2LoginIllustrationBorderedDark from '@images/pages/auth-v2-login-illustration-bordered-dark.png'
 import authV2LoginIllustrationBorderedLight from '@images/pages/auth-v2-login-illustration-bordered-light.png'
 import authV2LoginIllustrationDark from '@images/pages/auth-v2-login-illustration-dark.png'
 import authV2LoginIllustrationLight from '@images/pages/auth-v2-login-illustration-light.png'
 import authV2MaskDark from '@images/pages/misc-mask-dark.png'
 import authV2MaskLight from '@images/pages/misc-mask-light.png'
+import { VNodeRenderer } from '@layouts/components/VNodeRenderer'
+import { themeConfig } from '@themeConfig'
 
-const authThemeImg = useGenerateImageVariant(authV2LoginIllustrationLight, authV2LoginIllustrationDark, authV2LoginIllustrationBorderedLight, authV2LoginIllustrationBorderedDark, true)
-
-const authThemeMask = useGenerateImageVariant(authV2MaskLight, authV2MaskDark)
+const form = ref({
+  email: 'admin@demo.com',
+  password: 'admin',
+  remember: false,
+})
 
 const isPasswordVisible = ref(false)
 
-const email = ref('admin@demo.com')
-const password = ref('admin')
-const rememberMe = ref(false)
+const authThemeImg = useGenerateImageVariant(
+  authV2LoginIllustrationLight,
+  authV2LoginIllustrationDark,
+  authV2LoginIllustrationBorderedLight,
+  authV2LoginIllustrationBorderedDark,
+  true)
+
+const authThemeMask = useGenerateImageVariant(authV2MaskLight, authV2MaskDark)
 </script>
 
 <template>
@@ -29,8 +34,8 @@ const rememberMe = ref(false)
     class="auth-wrapper"
   >
     <VCol
-      lg="8"
-      class="d-none d-lg-flex"
+      md="8"
+      class="d-none d-md-flex"
     >
       <div class="position-relative auth-bg rounded-lg w-100 ma-8 me-0">
         <div class="d-flex align-center justify-center w-100 h-100">
@@ -42,16 +47,16 @@ const rememberMe = ref(false)
         </div>
 
         <VImg
-          :src="authThemeMask"
           class="auth-footer-mask"
+          :src="authThemeMask"
         />
       </div>
     </VCol>
 
     <VCol
       cols="12"
-      lg="4"
-      class="d-flex align-center justify-center"
+      md="4"
+      class="auth-card-v2 d-flex align-center justify-center"
     >
       <VCard
         flat
@@ -63,26 +68,12 @@ const rememberMe = ref(false)
             :nodes="themeConfig.app.logo"
             class="mb-6"
           />
-
           <h5 class="text-h5 font-weight-semibold mb-1">
-            Welcome to {{ themeConfig.app.title }}! 👋🏻
+            Welcome to <span class="text-capitalize">{{ themeConfig.app.title }}</span>! 👋🏻
           </h5>
           <p class="mb-0">
             Please sign-in to your account and start the adventure
           </p>
-        </VCardText>
-        <VCardText>
-          <VAlert
-            color="primary"
-            variant="tonal"
-          >
-            <p class="text-caption mb-2">
-              Admin Email: <strong>admin@demo.com</strong> / Pass: <strong>admin</strong>
-            </p>
-            <p class="text-caption mb-0">
-              Client Email: <strong>client@demo.com</strong> / Pass: <strong>client</strong>
-            </p>
-          </VAlert>
         </VCardText>
         <VCardText>
           <VForm @submit.prevent="() => {}">
@@ -90,19 +81,18 @@ const rememberMe = ref(false)
               <!-- email -->
               <VCol cols="12">
                 <VTextField
-                  v-model="email"
+                  v-model="form.email"
+                  autofocus
                   label="Email"
                   type="email"
-                  :rules="[requiredValidator, emailValidator]"
                 />
               </VCol>
 
               <!-- password -->
               <VCol cols="12">
                 <VTextField
-                  v-model="password"
+                  v-model="form.password"
                   label="Password"
-                  :rules="[requiredValidator]"
                   :type="isPasswordVisible ? 'text' : 'password'"
                   :append-inner-icon="isPasswordVisible ? 'tabler-eye-off' : 'tabler-eye'"
                   @click:append-inner="isPasswordVisible = !isPasswordVisible"
@@ -110,7 +100,7 @@ const rememberMe = ref(false)
 
                 <div class="d-flex align-center flex-wrap justify-space-between mt-2 mb-4">
                   <VCheckbox
-                    v-model="rememberMe"
+                    v-model="form.remember"
                     label="Remember me"
                   />
                   <a
@@ -132,7 +122,7 @@ const rememberMe = ref(false)
               <!-- create account -->
               <VCol
                 cols="12"
-                class="text-center"
+                class="text-center text-base"
               >
                 <span>New on our platform?</span>
                 <a
@@ -142,6 +132,7 @@ const rememberMe = ref(false)
                   Create an account
                 </a>
               </VCol>
+
               <VCol
                 cols="12"
                 class="d-flex align-center"
@@ -173,7 +164,4 @@ const rememberMe = ref(false)
 <route lang="yaml">
 meta:
   layout: blank
-  action: read
-  subject: Auth
-  redirectIfLoggedIn: true
 </route>
