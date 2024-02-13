e890f1c1d16d7f38a6f9463b2dbf678983980b8d
PruthviPraj00 <pruthviprajapati379@gmail.com>
2024-02-13 13:21:43 +0530

    refactor(init): added automation for tailwind

---
+++ tailwind/master.ts
@@ -0,0 +1,44 @@
+import * as fs from 'fs-extra';
+import * as path from 'path';
+
+interface CopyConfig {
+    source: string;
+    destination: string;
+}
+
+const initPath = '../../';
+
+const masterDaisy = `${initPath}master-daisyui`;
+const masterPreline = `${initPath}master-preline`;
+const destination = `${initPath}sneat-tailwindcss`;
+
+const sourcePaths: CopyConfig[] = [
+    { source: path.join(masterDaisy, 'src'), destination: path.join(destination, 'src') },
+    { source: path.join(masterPreline, 'src'), destination: path.join(destination, 'src', 'js') },
+    { source: path.join(masterPreline, 'node-scripts'), destination: path.join(destination, 'src', 'node-scripts') },
+    { source: path.join(masterPreline, 'plugin.js'), destination: path.join(destination,'src', 'plugin.js') },
+    { source: path.join(masterPreline, 'webpack.config.js'), destination: path.join(destination, 'webpack.config.js') },
+    { source: path.join(masterPreline, 'tsconfig.json'), destination: path.join(destination, 'tsconfig.json') }
+];
+
+async function copyFolders(paths: CopyConfig[]): Promise<void> {
+    try {
+        for (const { source, destination } of paths) {
+            console.log(`Copying folder from ${source} to ${destination}`);
+            await fs.copy(source, destination, { overwrite: true });
+            console.log(`Folder copied successfully from ${source} to ${destination}`);
+        }
+        console.log('Files copied successfully!');
+    } catch (error) {
+        console.error('An error occurred while copying folders:', error);
+    }
+}
+
+
+async function main() {
+    await copyFolders(sourcePaths);
+}
+
+main().catch(error => {
+    console.error('An error occurred during script execution:', error);
+});
